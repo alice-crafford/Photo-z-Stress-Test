f339c6d31a4717242dfd32e837b61d89447bc199
Alice Crafford <acraffor@andrew.cmu.edu>
2023-11-17 09:01:26 -0800

    script to run a test pipeline onnersc

--- Pipeline_Outputs/Test_Pipeline_Run_Script.py
+++ Pipeline_Outputs/Test_Pipeline_Run_Script.py
@@ -1,3 +1,3 @@
 import ceci 
-pr = ceci.Pipeline.read("global/homes/a/acraffor/Photo-z-Stress-Test/Pipeline_Outputs/specSelection_lsstErr_pzflow/'BOSS'_lsstErr_pzflow.yml"
+pr = ceci.Pipeline.read("global/homes/a/acraffor/Photo-z-Stress-Test/Pipeline_Outputs/BOSS_CMNN/BOSS_CMNN.yml"
 pr.run()
